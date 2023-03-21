ecfc7c97d6f9a814ab17e2b4b930b8ad2f65ee6f
Ana Carolina <carol.oliva.cruz@gmail.com>
2023-03-21 17:04:39 -0300

    feat: list comics structure

---
+++ src/components/ListComics/index.tsx
@@ -0,0 +1,21 @@
+const ListComics = () => {
+  return (
+    <section>
+      <li key="id">
+        <div>
+          <div>
+            <img src="img" alt="Quadrinho" />
+          </div>
+          <div>
+            <h4>name</h4>
+            <p>category</p>
+            <p>price</p>
+            <button type="button">Ver</button>
+          </div>
+        </div>
+      </li>
+    </section>
+  );
+};
+
+export default ListComics;
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "../../components/Header";
+import ListComics from "../../components/ListComics";
 
 const HomePage = () => {
   return (
     <>
       <Header />
+      <main>
+        <ListComics/>
+      </main>
     </>
   );
 };
