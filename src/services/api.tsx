ff43657c4f9297d2450e5ac501908f492550be0a
Ana Carolina <carol.oliva.cruz@gmail.com>
2023-03-24 13:14:01 -0300

    fix: endpoint

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 
 const apiService = axios.create({
-  baseURL: "http://gateway.marvel.com/v1/public/",
+  baseURL: "https://gateway.marvel.com/v1/public/",
   timeout: 10000,
 });
 
